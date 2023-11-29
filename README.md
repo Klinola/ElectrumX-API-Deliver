6e2691c8ea8af2aec2e2a58aff7597acce911f1a
Klinola <KelinerALOC@outlook.com>
2023-11-29 14:57:35 +0800

    Have a Nice DAY~

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # ElectrumX-API-Deliver
-A JS scirpt to manage the ElectrumX server API.
+A JS scirpt to manage the ElectrumX server API. 
+
+```
+sudo apt install nodejs npm -y
+npm install express
+npm install axios
+```
+
+Change the `targeUrl` in `app.js` to your ElectrumX server 
+
+The api keys are stored manually on `apiKey.json`, and here is an example 
+
+```
+{
+    "user1": "c1ed192d3927d0236992838ce85516ef",
+    "user2": "<another apikey>"
+}
+```
+
+You can use `generateApiKey.js` to generate a new key or use your own one. 
+The usage of `generateApiKey.js` is 
+```
+node generateApiKey.js
+```
+
+and you should copy the key to the `apiKey.json` and set a username for it. 
+
+
+Use the following command to start the API server 
+```
+node app.js
+```
+
+An example URL GET query would be like 
+```
+http://127.0.0.1:3000/proxy?apiKey=c1ed192d3927d0236992838ce85516ef
+``` 
+
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
 app.get('/', (req, res) => {
     res.send('ElectrumX API Server is Running...');
@@ -10,8 +11,15 @@ const fs = require('fs');
 
 
 function verifyApiKey(req, res, next) {
-    const apiKey = req.header('X-API-Key') || req.query.apiKey;
+    let apiKey = req.header('X-API-Key') || req.query.apiKey || '';
+    
     const validApiKeys = JSON.parse(fs.readFileSync('apiKeys.json'));
+    const slashIndex = apiKey.indexOf('/');
+    if (slashIndex > -1) {
+        apiKey = apiKey.substring(0, slashIndex);
+        console.log('Extracted API Key:', apiKey);
+    }
+    
 
     if (apiKey && Object.values(validApiKeys).includes(apiKey)) {
         next();
@@ -20,24 +28,39 @@ function verifyApiKey(req, res, next) {
     }
 }
 
+
 app.use((req, res, next) => {
     if (req.url.startsWith('/proxy')) {
-        const apiKeyIndex = req.url.indexOf('?apiKey=');
-        if (apiKeyIndex > -1) {
-            const path = req.url.substring(0, apiKeyIndex);
-            const queryParams = req.url.substring(apiKeyIndex);
-            req.url = path.replace('/proxy', '') + queryParams;
+        const apiKeyParamIndex = req.url.indexOf('?apiKey=');
+        if (apiKeyParamIndex > -1) {
+            const nextSlashIndex = req.url.indexOf('/', apiKeyParamIndex + '?apiKey='.length);
+            req.nextSlashIndex = nextSlashIndex > -1 ? nextSlashIndex : req.url.length;
+            console.log("SI: ", req.nextSlashIndex)
+            let queryParams = req.url.substring(apiKeyParamIndex, req.nextSlashIndex);
+            let pathAfterProxy = req.url.substring(req.nextSlashIndex);
+            req.trueUrl = `/proxy${pathAfterProxy}`
+
+            // 重写 URL
+            req.url = `/proxy${pathAfterProxy}${queryParams}`;
+            console.log("req.url: ", req.url)
         }
     }
     next();
 });
 
-
 app.use(verifyApiKey);
 
 
 app.use('/proxy', async (req, res) => {
-    const targetUrl = `http://<elextrumx-rpc-ip>:<port>${req.url}`;
+    let targetUrl;
+
+    if (req.nextSlashIndex && req.nextSlashIndex > -1) {
+        targetUrl = `http://<ElectumX RPC server IP>:<port>${req.trueUrl}`;
+        console.log("target url: ",targetUrl)
+    } else {
+        targetUrl = `http://<ElectumX RPC server IP>:<port>/proxy`;
+    }
+
     try {
         const electrumxResponse = await axios.get(targetUrl);
         res.send(electrumxResponse.data);
