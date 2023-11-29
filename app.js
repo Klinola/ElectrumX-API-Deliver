d4664b58245657e4ccbbe6175ef7e9b90537751c
Klinola <KelinerALOC@outlook.com>
2023-11-29 13:28:38 +0800

    Redirect

--- app.js
+++ app.js
@@ -6,29 +6,44 @@ app.get('/', (req, res) => {
 });
 
 
-const express = require('express');
 const fs = require('fs');
 
 
 function verifyApiKey(req, res, next) {
-    const apiKey = req.header('X-API-Key');
+    const apiKey = req.header('X-API-Key') || req.query.apiKey;
     const validApiKeys = JSON.parse(fs.readFileSync('apiKeys.json'));
 
     if (apiKey && Object.values(validApiKeys).includes(apiKey)) {
-        next(); 
+        next();
     } else {
-        res.status(401).send('Invalid API key!');
+        res.status(401).send('Invalid API Key!');
     }
 }
 
+app.use((req, res, next) => {
+    if (req.url.startsWith('/proxy')) {
+        const apiKeyIndex = req.url.indexOf('?apiKey=');
+        if (apiKeyIndex > -1) {
+            const path = req.url.substring(0, apiKeyIndex);
+            const queryParams = req.url.substring(apiKeyIndex);
+            req.url = path.replace('/proxy', '') + queryParams;
+        }
+    }
+    next();
+});
+
+
 app.use(verifyApiKey);
 
+
 app.use('/proxy', async (req, res) => {
+    const targetUrl = `http://<elextrumx-rpc-ip>:<port>${req.url}`;
     try {
-        const electrumxResponse = await axios.get(`http://<server-ip-address>:<port>${req.url}`);
+        const electrumxResponse = await axios.get(targetUrl);
         res.send(electrumxResponse.data);
     } catch (error) {
-        res.status(500).send('ElectrumX Server request failed');
+        console.log('Error:', error);
+        res.status(500).send('ElectrumX server request failed');
     }
 });
 
